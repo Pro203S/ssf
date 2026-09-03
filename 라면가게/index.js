67a9b563bda2315fb775aef607e8e36d16fef20f
Bomin Kim <xenonbomin54@gmail.com>
2026-09-03 20:54:26 +0900

    빈칸 가독성 수정.

--- 라면가게/index.js
+++ 라면가게/index.js
@@ -1,28 +1,29 @@
 let bgStatus = 0;
 
-document.addEventListener("[          ]", async () => {
-    const game = document.[          ]("#game");
-    const mainBg = document.[          ]("#mainBg");
-    const howToBtn = document.[          ]("#howToBtn");
-    const gameBtn = document.[          ]("#gameBtn");
+document.addEventListener("DOMContentLoaded", async () => {
+    const game = document.querySelector("#game");
+    const mainBg = document.querySelector("#mainBg");
+    const howToBtn = document.querySelector("#howToBtn");
+    const gameBtn = document.querySelector("#gameBtn");
 
     // 메인 화면
-    game.addEventListener("[          ]", () => {
+    game.addEventListener("click", () => {
         if (bgStatus !== 1) return;
 
         mainBg.src = "/assets/images/backgrounds/main.png";
-        howToBtn.style.display = [          ];
-        gameBtn.style.display = [          ];
+        howToBtn.style.display = "block";
+        gameBtn.style.display = "block";
         bgStatus = 0;
     });
 
     // 게임 방법
-    howToBtn.addEventListener("[          ]", () => {
+    howToBtn.addEventListener("click", () => {
         mainBg.src = "/assets/images/backgrounds/tutorial.png";
-        howToBtn.style.display = "[          ]";
-        gameBtn.style.display = "[          ]";
+        howToBtn.style.display = "none";
+        gameBtn.style.display = "none";
+
         setTimeout(() => {
-            // 바로 바꾸면 안떠서 0.1초 뒤에 바꾸기
+            // 바로 바꾸면 안 떠서 0.1초 뒤에 바꾸기
             bgStatus = 1;
         }, 100);
     });
@@ -34,12 +35,18 @@ document.addEventListener("[          ]", async () => {
     howToBtn.addEventListener("mouseenter", () => {
         steam.play();
     });
+
     gameBtn.addEventListener("mouseenter", () => {
         steam.play();
     });
 
     // BGM 재생 (위치, 반복 여부, BGM 길이)
-    const player = new AudioPlayer("/assets/sounds/bgm/main.mp3", true, 4.324);
+    const player = new AudioPlayer(
+        "/assets/sounds/bgm/main.mp3",
+        true,
+        4.324
+    );
+
     await player.load();
     player.play();
 });
